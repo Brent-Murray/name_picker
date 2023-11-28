ec78a5d1800f9df643b85450f50255f7b6e71e6f
Murray <brntmrry@ead.ubc.ca>
2023-11-28 10:55:12 -0800

    Update
    Update to automatically find optimal number of spaces

--- random_name_picker.R
+++ random_name_picker.R
@@ -1,6 +1,18 @@
 library(crayon)
 
-names = c("Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 10", "Group 100000")
+names = c("Group A: Mapping and monitoring wildlife habitate in the high arctic - Kianna & Dimitrios", 
+          "Group B: Use of drone based remote sensing in forestry - Fei, Xinyu & Heyuan", 
+          "Groub C: Remote sensing techniques to monitor oil spills - Sander & Valerie",
+          "Group D: Remote sensing and urban growth monitoring - Skyler & Nora",
+          "Group E: Using Landsat for wildfire risk assessment/monitoring - Michael & Rhythm",
+          "Group F: Remote sensing applications to quantify biodiversity in conservation/reforestation projects - Fitz & Colin",
+          "Group G: Remote sensing of landscape metrics - Wendy, Chenyao & Jessie",
+          "Group H: Glacial time series using remote sensing - Andrea & Axel",
+          "Group I: Applications of SPOT imagery - Stephani & Ashley",
+          "Group J: Nighttime satellite optical imagery for socioeconomic dynamics - Zhenpeng, Zechen, & Victor",
+          "Group K: Object based image classification - Aster, Xuan & Shijing", 
+          "Group L: Mapping kelp using google earth engine, drones, and lidar - Hannah, Han & Gwyn")
+
 colours = c("red", "green", "yellow", "blue", "magenta", "cyan", 
             "white", "silver")
 
@@ -8,37 +20,39 @@ quick_print <- function(names, colours){
   for (i in 1:50){
     x <- sample(names, 1)
     y <- sample(colours, 1)
+    max_length <- max(nchar(names)) + 20  # Calculate the maximum length needed
+    padding <- paste(rep(" ", max_length - nchar(x)), collapse = "")
     
     if (y == "red"){
-      cat(red(x, "    \r"))
+      cat(red(paste0(x, padding), "\r"))
     }
     
     if (y == "green"){
-      cat(green(x, "    \r"))
+      cat(green(paste0(x, padding), "\r"))
     }
     
     if (y == "yellow"){
-      cat(yellow(x, "    \r"))
+      cat(yellow(paste0(x, padding), "\r"))
     }
     
     if (y == "blue"){
-      cat(blue(x, "    \r"))
+      cat(blue(paste0(x, padding), "\r"))
     }
     
     if (y == "magenta"){
-      cat(magenta(x, "    \r"))
+      cat(magenta(paste0(x, padding), "\r"))
     }
     
     if (y == "cyan"){
-      cat(cyan(x, "    \r"))
+      cat(cyan(paste0(x, padding), "\r"))
     }
     
     if (y == "white"){
-      cat(white(x, "    \r"))
+      cat(white(paste0(x, padding), "\r"))
     }
     
     if (y == "silver"){
-      cat(silver(x, "    \r"))
+      cat(silver(paste0(x, padding), "\r"))
     }
     
     flush.console()
@@ -48,42 +62,45 @@ quick_print <- function(names, colours){
 
 name_picker <- function(names, colours){
   names2 <- names
+  max_length <- max(nchar(names)) + 20  # Calculate the maximum length needed
+  
   for (i in 1:length(names)){
     flush.console()
-    quick_print(names, colours)
+    quick_print(names, colours)  # Assuming this is a function you have defined
     x <- sample(names2, 1, replace=FALSE)
     y <- sample(colours, 1)
+    padding <- paste(rep(" ", max_length - nchar(x)), collapse = "")
     
     if (y == "red"){
-      cat(red(x, "              \r"))
+      cat(red(paste0(x, padding), "\r"))
     }
     
     if (y == "green"){
-      cat(green(x, "              \r"))
+      cat(green(paste0(x, padding), "\r"))
     }
     
     if (y == "yellow"){
-      cat(yellow(x, "              \r"))
+      cat(yellow(paste0(x, padding), "\r"))
     }
     
     if (y == "blue"){
-      cat(blue(x, "              \r"))
+      cat(blue(paste0(x, padding), "\r"))
     }
     
     if (y == "magenta"){
-      cat(magenta(x, "              \r"))
+      cat(magenta(paste0(x, padding), "\r"))
     }
     
     if (y == "cyan"){
-      cat(cyan(x, "              \r"))
+      cat(cyan(paste0(x, padding), "\r"))
     }
     
     if (y == "white"){
-      cat(white(x, "              \r"))
+      cat(white(paste0(x, padding), "\r"))
     }
     
     if (y == "silver"){
-      cat(silver(x, "              \r"a))
+      cat(silver(paste0(x, padding), "\r"))
     }
     
     flush.console()
